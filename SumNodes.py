fa295572badddf432d9d6cc943a3805348e14967
Linsey Briley <ljb26675@uga.edu>
2019-08-20 00:22:08 -0400

    Added python program that finds a pair of nodes in a BST that sum to a certain value.

---
+++ SumNodes.py
@@ -0,0 +1,55 @@
+# Python2 program that finds two nodes in a tree that sum equals k
+
+
+import math
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.right = None
+        self.left = None
+        
+        
+# insert method to create a binary tree
+def insert(root, data):
+    #if no root
+    if root is None:
+        return Node(data)
+    #if data is less than root data, insert it to left
+    if(data < root.data):
+        root.left = insert(root.left, data)
+    # if greater than, insert it to the right.
+    if(data > root.data): 
+        root.right = insert(root.right, data) 
+    return root 
+        
+def utility(root, sum, unsorted_set):
+    if root is None:
+        return False
+    if utility(root.left, sum, unsorted_set):
+        return True
+    if unsorted_set and (sum-root.data) in unsorted_set:
+        print("Pair Found: ({0}, {1})".format(sum-root.data, root.data)) 
+        return True
+    else:
+        unsorted_set.add(root.data)
+            
+    return utility(root.right, sum, unsorted_set)
+        
+def sumNodes(root, sum):
+    unsorted_set = set()
+    if(not utility(root,sum,unsorted_set)): 
+        print("Pair does not exist!") 
+        
+        
+# test functions
+if __name__=='__main__': 
+    root= None
+    root = insert(root,10) 
+    root = insert(root,5) 
+    root = insert(root,15) 
+    root = insert(root,11) 
+    root = insert(root,15) 
+    sum = 20
+    sumNodes(root, sum) 
